a861f41336dacfc1097cb82d0a3559cec8788afd
AsfaaKhan <khanzadiasfi0314@gmail.com>
2025-06-03 21:16:58 +0500

    Gemini Chat Agent

---
+++ main.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv
+import os
+from agents import Agent, AsyncOpenAI,OpenAIChatCompletionsModel, RunConfig, Runner
+import streamlit as st
+import asyncio
+
+load_dotenv()
+
+
+MODEL_NAME="gemini-2.0-flash"
+GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
+external_client=AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    )
+
+model=OpenAIChatCompletionsModel(
+    model=MODEL_NAME,
+    openai_client=external_client
+    )
+
+config=RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+    )
+assistant = Agent(
+    name="Assistant",
+    instructions="Your job is to resolve qurries",
+    model=model
+    )
+
+
+async def get_response(user_input):
+    result = await Runner.run(assistant,user_input,run_config=config)  
+    return result.final_output
+
+# Stremlit
+st.set_page_config(page_title="Gemini Chat Agent",page_icon="🧠",layout="centered")
+
+st.title("🧠 Gemini Chat App")
+
+user_input = st.text_input(" Enter The question : ")
+
+if st.button("Send"):
+
+    if user_input.strip() == "":
+        st.warning("Please enter a question.")
+
+    else:
+        with st.spinner("🗯 Thinking..."):
+            response = asyncio.run(get_response(user_input))
+            st.title("🎊 Your Answer: ")
+            st.write(response)
+
